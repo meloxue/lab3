825fc512c4820f42a61f4bb209f403d5570d7c62
meloxue <melodyxue1997@gmail.com>
2019-01-24 09:04:07 -0800

    updated website in lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,8 +10,48 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+
 	});
+
+	//from lab 2
+	$("a.thumbnail").click(projectClick);
+	
+/*function projectClick(e) {
+	console.log("Project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+}	
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+}
+*/
+function projectClick(e) {
+	e. preventDefault();
+	var containingProject = $(this).closest(".project");
+	//containingProject.append("<div class='project-description'<p>Description of the project.</p></div>");
+    var description = $(containingProject).find(".project-description");
+
+    if (description.length == 0) {
+	   $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	$(".project-description").fadeOut();
+	//description.html("<p>Stop clicking on me! You just did it at" + (new Date()) + "</p>");
+}
+  
+
+}
+
+
+
+//$('#myelement').click(changeText);
+//function changeText(event) {
+//	$(this).text("Changed text");
+//}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
